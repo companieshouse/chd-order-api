bf2cb831ce625c011a3e695ae26320c2dd7a188e
ethatcher83 <ethatcher@companieshouse.gov.uk>
2020-11-10 11:00:51 +0000

    resolve pr comments reformat model file export duplicate code into seperate method

--- src/test/java/uk/gov/companieshouse/chd/order/api/service/OrderServiceTest.java
+++ src/test/java/uk/gov/companieshouse/chd/order/api/service/OrderServiceTest.java
@@ -13,7 +13,6 @@ import uk.gov.companieshouse.chd.order.api.model.OrderDetails;
 import uk.gov.companieshouse.chd.order.api.model.OrderHeader;
 import uk.gov.companieshouse.chd.order.api.repository.OrderHeaderRepository;
 
-import java.time.LocalDateTime;
 import java.util.Collections;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -22,21 +21,13 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.when;
+import static uk.gov.companieshouse.chd.order.api.testUtils.ItemSetup.setUpMissingImageDeliveriesRequest;
 
 @ExtendWith(MockitoExtension.class)
 class OrderServiceTest {
     private static final String FH_CATEGORY = "accounts";
     private static final String FH_DATE = "2018-04-22";
     private static final String ITEM_COST = "3";
-    private static final String ID = "MID-462515-995726";
-    private static final String COMPANY_NUMBER = "00006400";
-    private static final String COMPANY_NAME = "THE GIRLS' DAY SCHOOL TRUST";
-    private static final String PAYMENT_REFERENCE = "payment xyz";
-    private static final LocalDateTime ORDERED_AT = LocalDateTime.parse("2020-10-26T10:20:46.058");
-    private static final String FILING_HISTORY_CATEGORY = "officer";
-    private static final String FILING_HISTORY_DATE = "2010-02-12";
-    private static final String FILING_HISTORY_DESCRIPTION = "change-person-director-company-with-change-date";
-    private static final String FILING_HISTORY_TYPE = "Filing History Test Type";
     private static final String FILING_HISTORY_BARCODE = "1111111";
     private static final String ENTITY_ID = "222222";
     private static final String PAYMENT_REFERENCE_APPENED_BARCODE = "payment xyz--1111111";
@@ -73,17 +64,7 @@ class OrderServiceTest {
 
     @Test
     void paymentRefHasBarcodeAppended() {
-        final MissingImageDeliveriesRequest midRequest = new MissingImageDeliveriesRequest();
-        midRequest.setId(ID);
-        midRequest.setCompanyNumber(COMPANY_NUMBER);
-        midRequest.setCompanyName(COMPANY_NAME);
-        midRequest.setPaymentReference(PAYMENT_REFERENCE);
-        midRequest.setOrderedAt(ORDERED_AT);
-        midRequest.setItemCost(ITEM_COST);
-        midRequest.setFilingHistoryCategory(FILING_HISTORY_CATEGORY);
-        midRequest.setFilingHistoryDate(FILING_HISTORY_DATE);
-        midRequest.setFilingHistoryDescription(FILING_HISTORY_DESCRIPTION);
-        midRequest.setFilingHistoryType(FILING_HISTORY_TYPE);
+        final MissingImageDeliveriesRequest midRequest = setUpMissingImageDeliveriesRequest();
         midRequest.setFilingHistoryBarcode(FILING_HISTORY_BARCODE);
 
         serviceUnderTest.saveOrderDetails(midRequest);
@@ -97,17 +78,7 @@ class OrderServiceTest {
 
     @Test
     void paymentRefHasEntityIdAppended() {
-        final MissingImageDeliveriesRequest midRequest = new MissingImageDeliveriesRequest();
-        midRequest.setId(ID);
-        midRequest.setCompanyNumber(COMPANY_NUMBER);
-        midRequest.setCompanyName(COMPANY_NAME);
-        midRequest.setPaymentReference(PAYMENT_REFERENCE);
-        midRequest.setOrderedAt(ORDERED_AT);
-        midRequest.setItemCost(ITEM_COST);
-        midRequest.setFilingHistoryCategory(FILING_HISTORY_CATEGORY);
-        midRequest.setFilingHistoryDate(FILING_HISTORY_DATE);
-        midRequest.setFilingHistoryDescription(FILING_HISTORY_DESCRIPTION);
-        midRequest.setFilingHistoryType(FILING_HISTORY_TYPE);
+        final MissingImageDeliveriesRequest midRequest = setUpMissingImageDeliveriesRequest();
         midRequest.setEntityID(ENTITY_ID);
 
         serviceUnderTest.saveOrderDetails(midRequest);
@@ -121,19 +92,9 @@ class OrderServiceTest {
 
     @Test
     void paymentRefHasEntityIdAppendedWhenBarcodeIsAlsoAvailable() {
-        final MissingImageDeliveriesRequest midRequest = new MissingImageDeliveriesRequest();
-        midRequest.setId(ID);
-        midRequest.setCompanyNumber(COMPANY_NUMBER);
-        midRequest.setCompanyName(COMPANY_NAME);
-        midRequest.setPaymentReference(PAYMENT_REFERENCE);
-        midRequest.setOrderedAt(ORDERED_AT);
-        midRequest.setItemCost(ITEM_COST);
-        midRequest.setFilingHistoryCategory(FILING_HISTORY_CATEGORY);
-        midRequest.setFilingHistoryDate(FILING_HISTORY_DATE);
-        midRequest.setFilingHistoryDescription(FILING_HISTORY_DESCRIPTION);
-        midRequest.setFilingHistoryType(FILING_HISTORY_TYPE);
-        midRequest.setFilingHistoryBarcode(FILING_HISTORY_BARCODE);
+        final MissingImageDeliveriesRequest midRequest = setUpMissingImageDeliveriesRequest();
         midRequest.setEntityID(ENTITY_ID);
+        midRequest.setFilingHistoryBarcode(FILING_HISTORY_BARCODE);
 
         serviceUnderTest.saveOrderDetails(midRequest);
 
