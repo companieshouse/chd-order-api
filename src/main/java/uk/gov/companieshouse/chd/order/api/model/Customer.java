c54041a7f9f22176b37830c6ece492902380e3da
hwilliams1 <hwilliams1@companieshouse.gov.uk>
2021-01-06 14:40:06 +0000

    Removed unused import to correct code smell

--- src/main/java/uk/gov/companieshouse/chd/order/api/model/Customer.java
+++ src/main/java/uk/gov/companieshouse/chd/order/api/model/Customer.java
@@ -4,7 +4,6 @@ import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
